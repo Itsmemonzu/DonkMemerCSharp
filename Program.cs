3554000608053b3f46c1fc7163b6150ee4a9661c
Monzu77 <90857410+Itsmemonzu@users.noreply.github.com>
2023-01-19 22:38:51 +0600

    Added Fortune Cookie command.
    Added new command: Fortune Cookie. It asks you if you want to eat a Fortune Cookie or not. If you say yes, it will tell you your Fortune.

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace DonkMemer
 
 
             // Arrays
-            String[] commandlist = {"8ball", "bal", "shoplist", "buy", "bet", "stop", "inventory", "beg"};
+            String[] commandlist = {"8ball", "bal", "shoplist", "buy", "bet", "stop", "inventory", "beg", "fortunecookie"};
 
             String[] EightBall = {"Yes", "No"};
 
@@ -24,6 +24,7 @@ namespace DonkMemer
 
             String[] begList = {"Random guy: Oh you poor little beggar, take 80$", "That guy you hate: ew get away", "LOLLL IMAGINE BEGGING", "You stink!", "*Random guy kicks you and runs away*", "Oh no! What a poor little creature... HAHAHA.... Here! Take 379$"};
 
+            String[] fortuneTeller = {"Do not hold onto things that require a tight grip.", "Don't come this far to only come this far.", "Vulnerability sounds like faith and looks like courage.", "Do it scared.", "Today it's up to you to create the peacefulness you long for.", "If you refuse to accept anything but the best, you very often get it.", "A smile is your passport into the hearts of others.", "Your high-minded principles spell success.", "Hard work pays off in the future, laziness pays off now.", "Change can hurt, but it leads a path to something better.", "Enjoy the good luck a companion brings you.", "People are naturally attracted to you.", "Hidden in a valley beside an open stream- This will be the type of place where you will find your dream.", "A chance meeting opens new doors to success and friendship.", "You learn from your mistakes... You will learn a lot today.", "If you have something good in your life, don't let it go!", "What ever you're goal is in life, embrace it visualize it, and for it will be yours.", "Your shoes will make you happy today.", "You cannot love life until you live the life you love.", "Be on the lookout for coming events; They cast their shadows beforehand.", "Land is always on the mind of a flying bird.", "The man or woman you desire feels the same about you.", "Meeting adversity well is the source of your strength.", "A dream you have will come true.", "Our deeds determine us, as much as we determine our deeds.", "Never give up. You're not a failure if you don't give up.", "You will become great if you believe in yourself.", "There is no greater pleasure than seeing your loved ones prosper.", "You already know the answer to the questions lingering inside your head.", "It is now, and in this world, that we must live.", "You must try, or hate yourself for not trying.", "You can make your own happiness.", "The greatest risk is not taking one.", "The love of your life is stepping into your planet this summer.", "Love can last a lifetime, if you want it to.", "Adversity is the parent of virtue.", "Serious trouble will bypass you.", "A short stranger will soon enter your life with blessings to share.", "Now is the time to try something new.", "Wealth awaits you very soon.", "If you feel you are right, stand firmly by your convictions.", "If winter comes, can spring be far behind?", "Keep your eye out for someone special.", "You are very talented in many ways.", "A stranger, is a friend you have not spoken to yet.", "A stranger, is a friend you have not spoken to yet.", "A new voyage will fill your life with untold memories.", "Your ability for accomplishment will follow with success.", "Nothing astonishes men so much as common sense and plain dealing.", "Its amazing how much good you can do if you dont care who gets the credit."};
 
             // Execution part
 
@@ -33,7 +34,7 @@ namespace DonkMemer
             {       
                 
                 Console.WriteLine("");
-                Console.WriteLine("Command list: 8ball, bal, shoplist, buy, bet, inventory, beg, stop");
+                Console.WriteLine("Command list: 8ball, bal, shoplist, buy, bet, inventory, beg, fortunecookie, stop");
                 Console.WriteLine("");
                 Console.WriteLine("Write a command: ");
                 Console.WriteLine("");
@@ -282,6 +283,31 @@ namespace DonkMemer
                     Console.WriteLine("");
                 }
 
+                //Fortune Cookie
+                if(cmd.Equals(commandlist[8]))
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Do you want to eat a Fortune Cookie?");
+                    Console.WriteLine("Yes or No?");
+                    Console.WriteLine("");
+                    string answer = Console.ReadLine();
+
+                    if(answer.Equals("Yes") || answer.Equals("yes"))
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Your fortune is:");
+                        Console.WriteLine("");
+                        Random rand = new Random();  
+                        int index = rand.Next(fortuneTeller.Length);  
+                        Console.WriteLine($"{fortuneTeller[index]}");
+                    }
+                    if(answer.Equals("No") || answer.Equals("no"))
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("You denied the Fortune Cookie.");
+                    }
+                }
+
                 //Stop
                 if(cmd.Equals(commandlist[5]))
                 {
@@ -290,6 +316,7 @@ namespace DonkMemer
                     writer.Close();
                     Console.WriteLine("Stopping...");
                     break;
+
                 }
 
             }
